ef58bee26617769c9b2bd4e08375d6392fd71cf4
Zachary Jia <jia199474@gmail.com>
2015-11-12 11:01:37 +0800

    add docker file

---
+++ Dockerfile.php
@@ -0,0 +1,18 @@
+FROM ubuntu:14.04
+MAINTAINER Zachary Jia <jia199474@gmail.com>
+
+RUN dpkg-divert --local --rename --add /sbin/initctl
+RUN ln -sf /bin/true /sbin/initctl
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update
+RUN apt-get -y upgrade
+
+RUN apt-get -y install apache2
+RUN apt-get -y install php5
+RUN apt-get -y install php5-curl
+
+CPOY * /var/www/html
+
+EXPOSE 80
