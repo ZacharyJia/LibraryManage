63d1650f597e4c194811508c22555f6e6348b009
Zachary <jia199474@gmail.com>
2015-11-18 14:49:54 +0800

    重命名

---
+++ src/app/Http/Middleware/LoginCheck.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jia19
+ * Date: 2015/11/18
+ * Time: 13:54
+ */
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Closure;
+
+class LoginCheck {
+
+    public function handle(Request $request, Closure $next)
+    {
+        $isLogin = $request->session()->get("isLogin");
+        $type = $request->session()->get("type");
+        if ($isLogin == true && $type == 0)
+        {
+            return redirect('admin/home');
+        }
+        if ($isLogin == true && type == 1)
+        {
+            return redirect('reader/home');
+        }
+        else
+        {
+            return redirect()->guest('login');
+        }
+    }
+
+
+}
